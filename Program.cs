f2ff74dd8d4c95a705bce531c776058d8bf8cdc9
CM <cheukman196@live.com>
2023-11-14 18:23:50 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AOOP_GroupProject_draft1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
